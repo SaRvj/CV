be9a793fb1436e3688c29a72bd6a58458bd3b192
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-08-30 16:42:23 +0200

    home implementation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,18 @@ import Navigation from "../components/Navigation";
 
 const Home = () =>{
   return(
-    <div>
+    <div className="home">
         <Navigation/>
+        <div className="homeContent">
+          <div className="content">
+            <h1> Satrana Ravelojaona</h1>
+            <h2> Développeur Front-End React</h2>
+            <div className="pdf">
+              <a href="./media/CV_Satrana_RAVELOJAONA" target="_blank"> Téléchargez CV</a>
+            </div>
+          </div>
+
+        </div>
     </div>
   )
 }
