356a1060cb2cd344ccbe01017e543c9fb62e403d
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-09-01 16:16:02 +0200

    add experiences

---
+++ src/components/knowledges/Experiences.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Experience = () => {
+    return (
+        <div className="experience">
+            <h3>Expériences</h3>
+            <div className="exp-1">
+                <h4>Assistante chargée d'études modélisation des transports - Cerema</h4>
+                <h5>03/2019 - 09/2019</h5>
+                <p> - Mise à jour de l'offre de transport routier à l'aide du SIG 
+                    - Gestion des ﬁchiers Shapes et statistiques routiers
+                    - Traitement de données sur Excel
+                    - Réalisation d'une étude sur les mobilités (région PACA) et établissement de cahier des charges
+                </p>
+            </div>
+
+            <div className="exp-2">
+                <h4>Coordinatrice communication RSE - Groupe VIVESCIA</h4>
+                <h5>04/2018 - 09/2018</h5>
+                <p> - Coordination du Safety Day du Groupe
+                    - Animation des groupes de travail Safety Day
+                    - Benchmark sur la politique RSE
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Experience;
--- src/pages/Knowledges.js
+++ src/pages/Knowledges.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Navigation from "../components/Navigation";
-import Experience from "../styles/components/knowledges/Experiences";
-import Hobbies from "../styles/components/knowledges/Hobbies";
-import Languages from "../styles/components/knowledges/Languages";
-import OtherSkills from "../styles/components/knowledges/OtherSkills";
+import Experience from "../components/knowledges/Experiences";
+import Hobbies from "../components/knowledges/Hobbies";
+import Languages from "../components/knowledges/Languages";
+import OtherSkills from "../components/knowledges/OtherSkills";
 
 const Knowledges = () =>{
   return(
