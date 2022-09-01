f5fc970c6b955142eb712661bd082d82652fdffb
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-09-01 17:08:25 +0200

    add hobbies

--- src/components/knowledges/Hobbies.js
+++ src/components/knowledges/Hobbies.js
@@ -2,8 +2,23 @@ import React from "react";
 
 const Hobbies = () => {
     return (
-        <div>
-           Hobbies 
+        <div className="hobbies">
+           <h3>Centres d'intérêt</h3> 
+           <ul>
+                <li className="hobby">
+                    <i className="fas fa-female"></i>
+                    <span>Danse de salon</span>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-music"></i>
+                    <i className="fa-solid fa-person-dress-simple"></i>
+                    <span>Chant chorale</span>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-plane"></i>
+                    <span>Voyage</span>
+                </li>
+           </ul>
         </div>
     );
 };
