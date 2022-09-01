e1705c05b789c7bf8fd37f9dbcc24ffd79fbd323
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-09-01 16:26:56 +0200

    add otherskills

--- src/components/knowledges/OtherSkills.js
+++ src/components/knowledges/OtherSkills.js
@@ -2,8 +2,20 @@ import React from "react";
 
 const OtherSkills = () => {
     return (
-        <div>
-           autres compétences
+        <div className="otherSkills">
+            <h3>Autres compétences</h3>
+            <div className="list">
+                <ul>
+                    <li><i className="fas fa-check-square"></i>Anglais professionnel</li>
+                    <li><i className="fas fa-check-square"></i>Git</li>
+                    <li><i className="fas fa-check-square"></i>Méthode agile</li>
+                </ul>
+                <ul>
+                    <li><i className="fas fa-check-square"></i>Figma</li>
+                    <li><i className="fas fa-check-square"></i>UI/UX Design</li>
+                    <li><i className="fas fa-check-square"></i>Accessibilité</li>
+                </ul>
+            </div>
         </div>
     );
 };
