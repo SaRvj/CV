9b2fcadfea4764b365a536561be2f387655e9b6e
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-10-10 17:39:42 +0200

    add handleRadio function, display of radio buttons on the bar

--- src/components/portfolio/ProjectList.js
+++ src/components/portfolio/ProjectList.js
@@ -4,15 +4,44 @@ import Project from './Project';
 
 export default class ProjectList extends Component {
     state = {
-        projects:portfolioData
+        projects:portfolioData,
+        radios: [
+            {id: 1, value: "Javascript"},
+            {id: 2, value: "CSS"},
+            {id: 3, value: "React"},
+            {id: 4, value: "HTML"}
+        ],
+        selectedRadio: 'javascript'
+    }
+
+    handleRadio = (event) => {
+        console.log(event.target.value);
+        let radio = event.target.value;
+        this.setState({selectedRadio : radio})
     }
 
     render() {
-        let {projects} = this.state;
+        let {projects, radios, selectedRadio} = this.state;
         return (
             <div className="portfolioContent">
                 <ul className="radioDisplay">
-
+                    {
+                        radios.map((radio) => {
+                            return(
+                                <li key={radio.id}>
+                                    <input 
+                                        type="radio"
+                                        name="raio"
+                                        checked={radio.value === selectedRadio}
+                                        value={radio.value}
+                                        id={radio.value}
+                                        onChange={this.handleRadio}
+                                    />
+                                    <label htmlFor={radio.value}>{radio.value}</label>
+                                </li>
+                            )
+                        })
+                    }
 
                 </ul>
 
