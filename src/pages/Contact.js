66a28584cabb888b4a26a2cec8c6dfc02359639c
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-10-17 16:24:03 +0200

    add contact content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,9 +3,20 @@ import Navigation from "../components/Navigation";
 
 const Contact = () =>{
   return(
-    <div>
+    <div className="contact">
       <Navigation/>
-    mes contacts
+      <div className="contactContent">
+        <div className="header"></div>
+        <div className="contactBox">
+          <h1> Contactez-moi </h1>
+          <ul>
+            <li>
+              <i className="fas fa-map-marker-alt"></i>
+              <span>Ile-de-France</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 };
