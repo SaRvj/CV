ec815a1552cf2d549d6c523a624d197c9b0d3514
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-11-16 17:23:28 +0100

    project data updates

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,38 +1,38 @@
 export const portfolioData = [
   {
     id: 1,
-    name: 'ipsum.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    name: 'Reservia',
+    languages: ['html','css'],
+    languagesIcons: ['fab fa-html5','fab fa-css3-alt'],
+    source: 'https://github.com/SaRvj/SaRvj-SatranaRavelojaona_2_13072021',
+    info: 'Reservia est un outil de planification de vacances. Ce site permet aux utilisateurs des trouver des hébergements et des activités dans la ville de leur choix. Ce projet introduit l\'utilisation de HTML et de CSS. Le but est de se familiariser à l\'utilisation de Flexbox et Grid. Aussi de connaitre l\'impotance de faire passer le code aux validateurs.',
     picture: './media/project1.JPG'
   },
   {
     id: 2,
-    name: 'lorem.fr',
-    languages: ['javascript','react'],
-    languagesIcons: ['fab fa-js','fab fa-react'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    name: 'Oh My Food!',
+    languages: ['html','css'],
+    languagesIcons: ['fab fa-html5','fab fa-css3-alt'],
+    source: 'https://github.com/SaRvj/SatranaRavelojaona_3_13072021',
+    info: 'OhMyFood repertorie les menus de restaurant gastronomique. En plus des systèmes classiques de réservation, les clients peuvent composer leur menu pour que les plats soient prêts à leur arrivée. Ce projet introduit à l\'utilisation de SASS. L\'utilisation de mixin et la création d\'animation via CSS sont les principaux objectifs de ce projet.',
     picture: './media/project2.JPG'
   },
   {
     id: 3,
-    name: 'dolorset.fr',
-    languages: ['javascript','react','css'],
-    languagesIcons: ['fab fa-js','fab fa-react','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    name: 'GameOn',
+    languages: ['javascript','css'],
+    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
+    source: 'https://github.com/SaRvj/SatranaRavelojaona_4_13072021',
+    info: 'GameOn est une PME spécialisée dans les concours de jeux, l\'objectif principal du site est d\'offir une interface facile d\'utilisation pour l\'inscription à leur concours. Ce projet introduit à l\'utilisation du langage de programmation Javascript. Dans ce projet, Javascript est utilisé pour la création de formulaire. L\'utilisation des Regex et écrire du code Javascript maintenable sont des objectifs de ce projet.',
     picture: './media/project3.JPG'
   },
   {
     id: 4,
-    name: 'laboriosam.fr',
-    languages: ['javascript','html','react'],
-    languagesIcons: ['fab fa-js','fab fa-html','fab fa-react'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    name: 'FishEye',
+    languages: ['javascript','html','css'],
+    languagesIcons: ['fab fa-js','fab fa-html','fab fa-css3-alt'],
+    source: 'https://github.com/SaRvj/SatranaRavelojaona_6_13072021',
+    info: 'FishEye est un site qui permet aux photographers indépendants de présenter leurs meilleurs travaux. Ce site combine l\'utilisation de HTML, SASS et Javascript afin de créer un site entièrement fonctionnelle, qui respecte également les conventions d\'accessibilité permettant ainsi de faciliter la navigation pour les utilisateurs d\'outils de navigation spécialisées.',
     picture: './media/project4.JPG'
   },
   {
